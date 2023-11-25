6f1151e327fdc097adc7372d9f89ee1e274d928c
franciscocbx <franciscocoronelb@gmail.com>
2023-11-25 17:16:35 -0300

    Bienvenida

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import InfoClinica from "./components/InfoClinica";
-
+import Bienvenida from "./components/Bienvenida";
 function App() {
   return (
     <>
       <Header />
+      <Bienvenida />
       <InfoClinica />
+      
     </>
   );
 }
---
+++ src/components/Bienvenida/index.jsx
@@ -0,0 +1,15 @@
+import logo from "../../assets/img/logo.png";
+
+
+
+export default function Bienvenida() {
+  return (
+    <div class="contenedorImagen">
+            <h1 class="itemImagen1">¡BIENVENIDOS!</h1>
+            <h3 class="itemImagen2">
+                Atención especializada en la salud y bienestar de las personas.
+            </h3>
+            <div class="itemImagen3"><a href="#registro">Registrate</a></div>
+        </div>
+  );
+}
