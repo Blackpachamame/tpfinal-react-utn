263ba3ab007b19219814ec89f66a70c14784ceba
Valexita <valeriamorenouruguay@gmail.com>
2023-11-25 17:40:05 -0300

    bienvenida2

--- src/components/Bienvenida/index.jsx
+++ src/components/Bienvenida/index.jsx
@@ -4,12 +4,12 @@ import logo from "../../assets/img/logo.png";
 
 export default function Bienvenida() {
   return (
-    <div class="contenedorImagen">
-            <h1 class="itemImagen1">¡BIENVENIDOS!</h1>
-            <h3 class="itemImagen2">
+    <div className="contenedorImagen">
+            <h1 className="itemImagen1">¡BIENVENIDOS!</h1>
+            <h3 className="itemImagen2">
                 Atención especializada en la salud y bienestar de las personas.
             </h3>
-            <div class="itemImagen3"><a href="#registro">Registrate</a></div>
+            <div className="itemImagen3"><a href="#registro">Registrate</a></div>
         </div>
   );
 }
